e10804db168ca99b58cb917c2f9ad09cb46d4679
IrinaS-77 <irinaiv635@gmail.com>
2020-11-24 03:49:58 +0300

    Update userAPI.js
    allwork

--- client/src/API/userAPI.js
+++ client/src/API/userAPI.js
@@ -16,33 +16,33 @@ export const loginAPI = (login, password) => {
 export const lentaAPI = (token, tags) => {
     
     if(token.length>4 && tags[0]){
-    return ms.post("/feed", {token, tags}).then(res=>res.data)
+    return ms.post("/feed/", {token, tags}).then(res=>res.data)
     }
     else if (token.length>4){
-        return ms.post("/feed", {token}).then(res=>res.data)
+        return ms.post("/feed/", {token}).then(res=>res.data)
     }
     else if (tags[0]){
-        return ms.post("/feed", {tags}).then(res=>res.data)
+        return ms.post("/feed/", {tags}).then(res=>res.data)
     }
-    else return ms.post("/feed", {}).then(res=>res.data)
+    else return ms.post("/feed/", {}).then(res=>res.data)
     
 }
 
 export const AnketaPost = (token, interests, ind) => {
-    return ms.post("/profile/interests", {token, interests, ind}).then(res=>res.data)
+    return ms.post("/profile/interests/", {token, interests, ind}).then(res=>res.data)
 }
 
 export const AnketaPut = (token, interests, ind) => {
-    return ms.put("/profile/interests", {token, interests,ind}).then(res=>res.data)
+    return ms.put("/profile/interests/", {token, interests,ind}).then(res=>res.data)
 }
 
 export const AnketaGet = (token) => {
-    return ms.get("/profile/interests", {params: {token}}).then(res=>res.data)
+    return ms.get("/profile/interests/", {params: {token}}).then(res=>res.data)
 }
 
 export const CardGet = (token, id) => {
-    if (token.length>3) {return ms.get("feed/"+id, {params: {token}}).then(res=>res.data) }
+    if (token.length>3) {return ms.get("feed/"+id+'/', {params: {token}}).then(res=>res.data) }
     else 
-    return ms.get("feed/"+id, {}).then(res=>res.data)
+    return ms.get("feed/"+id+'/', {}).then(res=>res.data)
 }
 
